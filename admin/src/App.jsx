ea8e6a5a2a7e59c1e2968fc42980d0bb4171487b
nbd01 <divya.nb@in.pega.com>
2025-01-21 20:10:27 +0530

    admin login navbar changes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,23 +2,39 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPopup from './components/LoginPopup/LoginPopup.jsx';
 import Dashboard from './components/Dashboard/Dashboard.jsx';
-import { BrowserRouter } from 'react-router-dom'
+import List from './pages/List/List.jsx';
+import Sidebar from './components/Sidebar/Sidebar.jsx';
+import Navbar from './components/Navbar/Navbar'
+import Add from './pages/Add/Add'
+import Orders from './pages/Orders/Orders'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-    const [showLogin, setShowLogin] = useState(true); // State to control LoginPopup visibility
+    const [showLogin, setShowLogin] = useState(true);
 
     return (
-
-            <Routes>
-                {/* Default route points to LoginPopup */}
-                <Route
-                    path="/"
-                    element={showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <Navigate to="/dashboard" />}
-                />
-                {/* Other routes */}
-                <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-
+        <Router>
+            {showLogin ? (
+                <LoginPopup setShowLogin={setShowLogin} />
+            ) : (
+                <div className='app'>
+                <ToastContainer />
+                <Navbar />
+                <hr />
+                <div className="app-content">
+                  <Sidebar />
+                  <Routes>
+                    <Route path="/add" element={<Add />} />
+                    <Route path="/list" element={<List />} />
+                    <Route path="/orders" element={<Orders />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/" element={<Navigate to="/dashboard" replace />} />
+                  </Routes>
+                </div>
+              </div>
+            )}
+        </Router>
     );
 }
 
